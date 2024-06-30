c78b8de3f295e4130670ff561bfac1fa216ccc39
SachPlayZ <ssachinsingh99@gmail.com>
2024-07-01 03:58:33 +0530

    Goooo

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,21 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    disable: process.env.NODE_ENV === "development",
-    workboxOptions: {
-      disableDevLogs: true,
-    },
-    // ... other options you like
-  });
-  
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    // ... other options you like
-  };
-  
-  module.exports = withPWA(nextConfig);
-  
+import withPWA from "@ducanh2912/next-pwa";
+
+const pwaConfig = {
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other options you like
+};
+
+export default withPWA(pwaConfig)(nextConfig);
